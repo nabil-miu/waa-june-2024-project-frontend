2b76a585198d81af85c2154d4502ae8b03f6a7d3
Reza Un Nabi <reza.newgen@gmail.com>
2024-07-18 11:55:29 -0500

    change axios call

--- src/services/ResourceCategoryService.js
+++ src/services/ResourceCategoryService.js
@@ -1,32 +1,12 @@
 import axiosInstance from '../AxiosConfig';
-import axios from 'axios';
 
-const baseURL = 'http://localhost:8080/api/v1';
 
-const getResourceCategories = () => {
-  return axios.get(baseURL+'/categories/all');
+const resourceCategories = {
+  getResourceCategories: () => axiosInstance.get('/categories/all'),
+  getResourceCategory: (id) => axiosInstance.get(`/categories/${id}`),
+  createResourceCategory: (resourceCategory) => axiosInstance.post('/categories', resourceCategory),
+  updateResourceCategory: (id, resourceCategory) => axiosInstance.put(`/categories/${id}`, resourceCategory),
+  deleteResourceCategory: (id) => axiosInstance.delete(`/categories/${id}`),
 };
 
-const getResourceCategory = (id) => {
-  return axios.get(baseURL+`/categories/${id}`);
-};
-
-const createResourceCategory = (resourceCategory) => {
-  return axios.post(baseURL+'/categories', resourceCategory);
-};
-
-const updateResourceCategory = (id, resourceCategory) => {
-  return axios.put(baseURL+`/categories/${id}`, resourceCategory);
-};
-
-const deleteResourceCategory = (id) => {
-  return axios.delete(baseURL+`/categories/${id}`);
-};
-
-export default {
-  getResourceCategories,
-  getResourceCategory,
-  createResourceCategory,
-  updateResourceCategory,
-  deleteResourceCategory,
-};
+export default resourceCategories;
