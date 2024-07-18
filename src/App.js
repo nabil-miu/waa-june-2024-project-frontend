7275ca10d0906d6125125db51c219558bacd2ac4
Darkhan Zhursin <112369989+darkhanzhursin@users.noreply.github.com>
2024-07-18 08:34:24 -0500

    13 events frontend (#21)
    * Users
    
    * events initial
    
    * event service
    
    * event component
    
    * Link
    
    * EVENT CRUD FIX
    
    * redundant
    
    * redundant component
    
    ---------
    
    Co-authored-by: Mohamed Nabil <mohamedmenasy@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-import {Route, Routes} from "react-router";
-import React, {useEffect} from "react";
+import "./App.css";
+import { Route, Routes } from "react-router";
+import React, { useEffect } from "react";
 import Dashboard from "./Dashboard";
 import Users from "./components/Users";
+import Events from "./components/Events";
 import Admins from "./components/Admins";
 
 function App() {
-    useEffect(() => {
-        document.title = "WAA545 - Project - React";
-    }, []);
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/users" element={<Users/>}/>
-                <Route path="/admins" element={<Admins />} />
-            </Routes>
-        </div>
-    );
+  useEffect(() => {
+    document.title = "WAA545 - Project - React";
+  }, []);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/admins" element={<Admins />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
