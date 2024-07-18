489812bcbdef15ec4abf7d6c5c0b309955d33bc1
Darkhan Zhursin <daratechn@gmail.com>
2024-07-17 21:26:05 -0500

    inital

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <Link to="/users"><h2>Users</h2></Link>
-            <Link to="/admins"><h2>Admins</h2></Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Link to="/users">
+        <h2>Users</h2>
+      </Link>
+      <Link to="/events">
+        <h2>Events</h2>
+      </Link>
+      <Link to="/admins">
+        <h2>Admins</h2>
+      </Link>
+      <Link to="/resources">
+        <h2>"Resources"</h2>
+      </Link>
+    </div>
+  );
 }
 
 export default Dashboard;
